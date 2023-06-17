ba4f78f39e85b2eaaf008f4aad108a0a33aef79f
Yuliya Zozulya <djulia@ukr.net>
2023-06-18 01:23:05 +0300

    adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
 import Link from "next/link";
+import s from "../styles/404.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>This page doesn't exist</h1>
-      <p>
-        Go to <Link href="/">Home Page</Link>
-      </p>
-    </div>
+    <main>
+      <div className="container">
+        <h1 style={{ marginBottom: "20px", textAlign: "center" }}>
+          This page doesn't exist
+        </h1>
+        <p style={{ textAlign: "center" }}>
+          Go back to{" "}
+          <Link href="/" className={s.linkAccent}>
+            Home Page
+          </Link>
+        </p>
+      </div>
+    </main>
   );
 };
 export default NotFoundPage;
