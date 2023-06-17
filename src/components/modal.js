04c8eb568cf2fc852ff311cdc34981c3a8938163
Yuliya Zozulya <djulia@ukr.net>
2023-06-18 01:11:13 +0300

    adding modal markup

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,8 @@
 import { createPortal } from "react-dom";
 import { useState, useEffect, useRef } from "react";
 import { useFormik } from "formik";
+import Button from "@/components/button";
+import s from "../styles/Modal.module.css";
 
 const Modal = ({ onModalClose }) => {
   const [mounted, setMounted] = useState(false);
@@ -22,61 +24,80 @@ const Modal = ({ onModalClose }) => {
   });
   return mounted && ref.current
     ? createPortal(
-        <div
-          style={{
-            display: "block",
-            position: "fixed",
-            top: "0",
-            left: "0",
-            paddingTop: "100px",
-            width: "100%",
-            height: "100%",
-            overflow: "auto",
-            backgroundColor: "rgba(0,0,0,0.4)",
-            fontSize: "46px",
-          }}
-        >
-          <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="id">ID</label>
-            <input
-              id="id"
-              name="id"
-              type="number"
-              onChange={formik.handleChange}
-              value={formik.values.id}
-            />
-            <label htmlFor="topic">Topic</label>
-            <input
-              id="topic"
-              name="topic"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.topic}
-            />
-            <label htmlFor="question_name">Question Name</label>
-            <input
-              id="question_name"
-              name="question_name"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.question_name}
-            />
-            <label htmlFor="question_comment">Question comment</label>
-            <input
-              id="question_comment"
-              name="question_comment"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.question_comment}
-            />
-            <button type="submit">Submit</button>
-            <button type="reset" onClick={() => formik.resetForm()}>
-              Reset
-            </button>
-            <button type="button" onClick={onModalClose}>
-              Cancel
-            </button>
-          </form>
+        <div className={s.overlay}>
+          <section className="container">
+            <h2 className={s.section_title}>Actions form</h2>
+            <form onSubmit={formik.handleSubmit} className={s.form}>
+              <h3 className={s.form_title}>Edit/Create questions</h3>
+              <div className={s.inputBox}>
+                <label htmlFor="id" className={s.label}>
+                  ID
+                </label>
+                <input
+                  id="id"
+                  name="id"
+                  type="number"
+                  onChange={formik.handleChange}
+                  value={formik.values.id}
+                  className={s.input}
+                />
+              </div>
+              <div className={s.inputBox}>
+                <label htmlFor="topic" className={s.label}>
+                  Topic
+                </label>
+                <input
+                  id="topic"
+                  name="topic"
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values.topic}
+                  className={s.input}
+                />
+              </div>
+              <div className={s.inputBox}>
+                <label htmlFor="question_name" className={s.label}>
+                  Question Name
+                </label>
+                <input
+                  id="question_name"
+                  name="question_name"
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values.question_name}
+                  className={s.input}
+                />
+              </div>
+              <div className={s.inputBox}>
+                <label htmlFor="question_comment" className={s.label}>
+                  Question comment
+                </label>
+                <input
+                  id="question_comment"
+                  name="question_comment"
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values.question_comment}
+                  className={s.input}
+                />
+              </div>
+              <div className={s.bottomBox}>
+                <Button type="submit" text="Submit" btnclass="btn" />
+                <Button
+                  type="reset"
+                  text="Clear"
+                  btnclass="btn"
+                  handleClick={() => formik.resetForm()}
+                />
+                <Button
+                  type="button"
+                  text="Cancel"
+                  btnclass="btn"
+                  handleClick={onModalClose}
+                />
+              </div>
+            </form>
+          </section>
         </div>,
         ref.current
       )
